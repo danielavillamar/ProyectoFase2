1dd231fd292e778507fe51c6b47b15ee03a28e85
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-03-17 15:11:06 -0600

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+// DANIELA VILLAMAR 19086
+// MIRKA MONZON
+// SEBASTIAN MALDONADO
+
+package application;
+
+public interface Calculator {
+
+	public String Calculate(String num1 , String num2 , String op);
+	
+}
