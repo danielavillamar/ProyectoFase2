767a00597dc5ac408fca03feed133889a1d0f489
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-03-20 14:39:55 -0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+package application;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+// DANIELA VILLAMAR 19086
+// MIRKA MONZON 18139
+// SEBASTIAN MALDONADO 18003
+// PROYECTO FASE 2
+// FECHA DE ENTREGA: 20/03/2020
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,1013.0,514.0);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
